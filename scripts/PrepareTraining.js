f36ad7f36605aa20a93f551f03c11f26bf86b322
Marcus Aurén <auren@student.chalmers.se>
2018-04-11 13:55:03 +0200

    Going to try to do stuff on server

--- scripts/PrepareTraining.js
+++ scripts/PrepareTraining.js
@@ -13,10 +13,10 @@ const pg = require('pg')
 
 
 const client = new pg.Client({
-		user: 'nodejs',
+		user: 'postgres',
 		host: 'localhost',
 		database: 'postgres',
-		password: 'NodeSQLServer',
+		password: 'databasen',
 		port: 5432,
 })
 client.connect()
