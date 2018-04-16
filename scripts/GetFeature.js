ccb201deda437f41b98adae4ba61cb97e3d675ba
Marcus Aurén <auren@student.chalmers.se>
2018-04-16 11:09:38 +0200

    added sql queries to script

--- scripts/GetFeature.js
+++ scripts/GetFeature.js
@@ -11,7 +11,6 @@ var client_secret = 'c66c8a77642c44d89b78c6a9d15efa56' // Your secret
 // Connecting to postgresSQL
 const pg = require('pg')
 
-
 const client = new pg.Client({
 		user: 'postgres',
 		host: 'localhost',
@@ -19,20 +18,6 @@ const client = new pg.Client({
 		password: 'databasen',
 		port: 5432,
 })
-
-client.connect()
-
-var song_id
-
-client.query('SELECT songid FROM songdata', (err, res) => {
-  	song_id = res.rows
-	for( i = 0; i < song_id.length; i++){
-		getFeatures(song_id[i]);
-	}
-	client.end()
-})
-
-
 
 // your application requests authorization
 var authOptions = {
@@ -46,6 +31,20 @@ var authOptions = {
   json: true
 }
 
+/* --------------------------------------------------------------
+----- Start of Script
+-----------------------------------------------------------------  */
+
+client.connect()
+
+var song_id
+
+client.query('SELECT songid FROM songdata', (err, res) => {
+  song_id = res.rows
+	for( i = 0; i < song_id.length; i++){
+		getFeatures(song_id[i]);
+	}
+})
 
 function getFeatures(song_id){
 	 console.log(song_id)
@@ -62,105 +61,20 @@ function getFeatures(song_id){
 	       json: true
 	     }
 	     request.get(options, function(error, response, body) {
-//	 		tempo = body.tempo
-	 		console.log(body.tempo)
-	 		console.log(body.energy)
-	 		console.log(body.valence)
-	 		console.log(body.loudness)
+	 				console.log(body.tempo)
+	 				console.log(body.mode)
+	 				console.log(body.loudness)
+					updateFeatureInDB(song_id, body.tempo, body.mode, body.loudness)
 	     })
 	   }
 	 })
 }
-/*
-var tempo
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/audio-features/' + song_id,
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    }
-    request.get(options, function(error, response, body) {
-		tempo = body.tempo
-		console.log(body.tempo)
-		console.log(body.energy)
-		console.log(body.valence)
-		console.log(body.loudness)
-    })
-  }
-})
-*/
-/*
-var ids = []
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/sordi11/playlists/5Lyb7YeIvfTTEFYdludB2e/tracks',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    }
-
-    request.get(options, function(error, response, body) {
-			console.log("body")
-			console.log(body.next)
-			next = body.next
-			console.log(next)
-
-			for (i = 0; i < 10; i++){
-					//console.log(body.items[i])
-					//console.log(body.items[i].track.id)
-					ids[i] = body.items[i].track.id
-
-					console.log(body.items[i].track.tempo)
-					console.log(body.items[i].track.mode)
-					console.log(body.items[i].track.loudness)
-
-			}
-			console.log(ids)
-
-			client.connect()
-			const queryText = "INSERT INTO songdata VALUES ($1,100000000)"
-			client.query(queryText, [ids[0]], (err, res) => {
-			  //var song_id = res.rows[0].songid
-			  //console.log(res.rows[0].songid);
-			  client.end()
-
-			})
+function updateFeatureInDB(songid, tempo, mode, loudness){
+		const text = "UPDATE tensordata SET tempo = ($2), mode = ($3), loudness = ($4) WHERE songid = ($1)"
+		client.query(text, [songid, tempo, mode, loudness], (err, res) => {
 		})
-	}
-})
 
-/*
-const text = "UPDATE tensordata SET tempo = ($1) WHERE songid = ($2)"
+}
 
-const client2 = new pg.Client({
-		user: 'nodejs',
-		host: 'localhost',
-		database: 'postgres',
-		password: 'NodeSQLServer',
-		port: 5432,
-})
-
-
-client2.connect()
-
-client2.query(text, [tempo, song_id], (err, res) => {
-	if (err) {
-		console.log(err.stack)
-	} else {
-		console.log(res)
-	}
-  client2.end()
-})
-*/
+client.end()
